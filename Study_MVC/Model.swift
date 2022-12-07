bd060f85fd0136d817c0744e5db8097da0913633
junghun bak <wjdgnsi79@naver.com>
2022-12-07 10:34:00 +0900

    step1

---
+++ Study_MVC/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Study_MVC
+//
+//  Created by ALOM on 2022/12/07.
+//
+
+import Foundation
+import UIKit
+
+class countPrint {
+    
+   static var answer = "Hello world!"
+    
+}
--- Study_MVC/ViewController.swift
+++ Study_MVC/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lableName: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeName(_ sender: UIButton) {
+        
+        lableName.text = countPrint.answer
+        
+    }
+    
 }
 
